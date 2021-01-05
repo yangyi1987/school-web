47ead17efeb14246ea60719fd4d58b236e6cfd31
yangyi1987 <1806147282@qq.com>
2021-01-05 16:59:48 +0800

    fix: 封装 axios 方法

--- src/http/axiox.ts
+++ src/http/axiox.ts
@@ -1,26 +1,34 @@
 import axios, { AxiosPromise, AxiosRequestConfig } from "axios";
+import { config } from "vue/types/umd";
 
 axios.defaults.baseURL = "http://localhost:3000";
-axios.defaults.headers.common['Authorization'] = "12132";
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.common["Authorization"] = "12132";
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
 
-// axios.request({
-//   url: "http://localhost:3000",
-//   timeout: 1000,
-//   // 跨域请求 需要带凭证
-//   withCredentials: true
-// });
-
+// get 方法
 export function get(
   url: string,
   config?: AxiosRequestConfig
 ): Promise<AxiosPromise> {
   return axios.get(url, config);
 }
+
+// post 方法
 export function post(
   url: string,
   params?: Record<string, any> | number | string,
   config?: AxiosRequestConfig
 ): Promise<AxiosPromise> {
   return axios.post(url, params, config);
+}
+
+// patch 方法
+export function update(url: string, params: any, config?: AxiosRequestConfig): Promise<AxiosPromise> {
+  return axios.patch(url, params, config)
+}
+
+// delete 方法
+export function remove(url: string, config?: AxiosRequestConfig) {
+  return axios.delete(url, config);
 }
